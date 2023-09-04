8971d0a5518b2c7c9432615bf2bb7eed8e6398e5
unknown <runemaster52@gmail.com>
2023-09-03 23:19:02 -0400

    added animations

---
+++ src/lib/project.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let Title = ""
+    export let Desc = ""
+    export let Langs = ""
+
+    Langs = Langs.split(" ");
+
+</script>
+
+<a
+    class="flex flex-col justify-between block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-white/10 hover:shadow-white/10"
+    href="https://github.com/Hublove/PhotographyPortfolio"
+    data-aos="zoom-out" 
+    data-aos-once="true"
+    data-aos-easing="ease-out-quart"
+>
+    <div class="">
+        <h2 class="mt-4 mb-4 text-3xl font-bold text-white ">{Title}</h2>
+
+        <p class="mt-1 text-sm text-gray-300">
+            {Desc}
+        </p>
+    </div>
+            
+    <div class="flex mt-4 ">
+        {#each Langs as Lang}
+            <p class="rounded-lg py-2 px-2 border bg-white text-gray-900  w-max mr-3">{Lang}</p>
+        {/each}
+    </div>
+</a>
---
+++ src/lib/skill.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let lang = ""
+    export let svg
+</script>
+
+<div
+    class="flex hover:invert justify-center rounded-xl p-4 shadow-sm bg-white border border-2 border-gray-900" 
+    data-aos="zoom-out-left" 
+    data-aos-once="true"
+    data-aos-easing="ease-out-quart"
+
+    
+>
+    <span class="inline-block rounded-lg bg-white m3">
+        <img class="h-10 w-10" src="{svg}">
+    </span>
+
+
+
+    <h2 class="self-center  mx-2 font-bold text-lg">{lang}</h2>
+</div>
